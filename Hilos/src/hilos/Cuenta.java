6eb29c94d1101fcc9ee2bd18be38267659a5fc19
BillVR <100880880+BillVR@users.noreply.github.com>
2022-12-23 03:39:14 -0500

    Add files via upload

---
+++ Hilos/src/hilos/Cuenta.java
@@ -0,0 +1,12 @@
+package hilos;
+
+public class Cuenta {
+	private double saldo = 500000;
+	public Double getSaldo() {
+	return saldo;
+	}
+	public void retirar(Double cantidad) {
+	saldo -= cantidad;
+	}
+}
+
